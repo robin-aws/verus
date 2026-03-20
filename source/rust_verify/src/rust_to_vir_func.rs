77b6bd74a1e00582018bb1f9b2cce3ecbe986d84
Travis Hance <tjhance7@gmail.com>
2026-03-20 12:52:02 +0100

    also special case tracked_take

--- source/rust_verify/src/rust_to_vir_func.rs
+++ source/rust_verify/src/rust_to_vir_func.rs
@@ -259,6 +259,7 @@ fn handle_autospec<'tcx>(
                     exec_allows_no_decreases_clause: false,
                     ignore_outside_new_mut_ref: functionx.attrs.ignore_outside_new_mut_ref,
                     tracked_swap: false,
+                    tracked_take_option: false,
                 }),
                 body: Some(ret_clause.clone()),
                 extra_dependencies: functionx.extra_dependencies.clone(),
@@ -1316,6 +1317,7 @@ fn make_attributes<'tcx>(
         },
         ignore_outside_new_mut_ref,
         tracked_swap: vattrs.tracked_swap,
+        tracked_take_option: vattrs.tracked_take_option,
     };
     Ok(Arc::new(fattrs))
 }
